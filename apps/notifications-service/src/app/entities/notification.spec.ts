4ae4042aa0381b8c60c4105196f5ea2941a67742
Pedro Moreno <nennogabriel@gmail.com>
2022-12-15 14:29:42 -0300

    aula 2

--- apps/notifications-service/src/app/entities/notification.spec.ts
+++ apps/notifications-service/src/app/entities/notification.spec.ts
@@ -10,4 +10,15 @@ describe(`Notification`, () => {
     });
     expect(content).toBeTruthy();
   });
+
+  it('should be able to create a notification mark as read', () => {
+    const content = new Notification({
+      content: new Content('Hello World'),
+      category: 'test',
+      recipientId: '123',
+      readAt: new Date(),
+    });
+    expect(content).toBeTruthy();
+    expect(content.readAt).toBeTruthy();
+  });
 });
